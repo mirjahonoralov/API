b07193ace0e80a68d3476180aeed34f19c107e87
mirjahon <mirjahonoralov@gmail.com>
2022-03-24 08:39:13 +0500

    edited

--- src/jsonplaceholderproject/components/PaginationComp.jsx
+++ src/jsonplaceholderproject/components/PaginationComp.jsx
@@ -37,7 +37,7 @@ const PaginationComp = ({ setPageNum, clickNext, clickPrev }) => {
     setPageNum(10);
   };
 
-  useEffect(() => newNums.length && setNums(newNums), [clicked]);
+  useEffect(() => newNums.length && setNums(newNums), [clicked, newNums]);
 
   return (
     <Pagination>
